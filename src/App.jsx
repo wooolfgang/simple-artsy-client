5848beb239c7412dc71d6916a54b2f482968c85e
Li Arolf Rey <liarolfrrey@gmail.com>
2019-05-18 11:28:33 +0800

    feat: Add routing and theme in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import GlobalStyle, { theme } from './theme/globalStyle';
+import Feed from './pages/Feed';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src={logo}
-          classNam
-          e="App-logo"
-          alt="logo"
-        />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <>
+          <GlobalStyle />
+          <Route exact path="/" component={Feed} />
+        </>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/globalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const theme = {
+  color: {
+    c1: '#011627',
+    c2: '#FF0022',
+    c3: '#41EAD4',
+    c4: '#FDFFFC',
+    c5: '#B91372',
+  },
+};
+
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
+
+export default GlobalStyle;
